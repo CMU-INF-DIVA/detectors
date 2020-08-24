d489cbe1cac77ec64016b21ba86a48029cd010eb
Lijun Yu <lijun@lj-y.com>
2020-08-24 18:12:06 -0400

    viz

--- visualizer.py
+++ visualizer.py
@@ -1,13 +1,17 @@
-import matplotlib as mpl
 import numpy as np
 import torch
 from detectron2.utils.visualizer import Visualizer as dt_visualizer
 from matplotlib import pyplot as plt
 
 from .base import Detection, ObjectType
+from .color import ColorManager
 
 
 class Visualizer(object):
+
+    def __init__(self):
+        self.color_manager = ColorManager()
+        self.object_types = ObjectType
 
     def draw(self, image: torch.Tensor, detection: Detection,
              show: bool = True, **show_args):
@@ -44,10 +48,22 @@ class Visualizer(object):
             score = detection.detection_scores[obj_i] * 100
             label = '%s-%s %.0f%%' % (obj_type, obj_id, score)
             labels.append(label)
+            x0, y0 = detection.image_boxes[obj_i, :2].type(torch.int)
+            x1, y1 = detection.image_boxes[obj_i, 2:].ceil().type(torch.int)
+            roi = image_rgb[y0:y1, x0:x1]
+            color = self.color_manager.get_color((obj_type, obj_id), roi)
+            colors.append(color)
             mask = [np.array([0, 0])]
             if detection.has('image_masks'):
                 mask = detection.image_masks[obj_i].numpy()
             masks.append(mask)
+        if detection.has('image_locations'):
+            keypoints = np.empty((len(detection), 1, 3))
+            keypoints[:, 0, :2] = detection.image_locations
+            keypoints[:, 0, 2] = 1
+        else:
+            keypoints = None
         visualizer.overlay_instances(
-            masks=masks, boxes=detection.image_boxes, labels=labels)
+            masks=masks, boxes=detection.image_boxes, labels=labels,
+            keypoints=keypoints, assigned_colors=colors)
         return colors
